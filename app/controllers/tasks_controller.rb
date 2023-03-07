c34fbc6ac64bd05d416f6b2c1d02bb37b351b7d4
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-07 13:28:07 +0800

    Added view for specific task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+    if @task.completed == false
+      @label = 'This task is not completed yet'
+      @label
+    else
+      @label = 'This task is completed'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: :index
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks/new', to: 'tasks#new', as: :new_task
 end
